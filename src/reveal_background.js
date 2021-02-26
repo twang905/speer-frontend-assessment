a35e1ec7d20b1c7a7261796fb953518044cb44bf
Thomas Wang <77583346+twang905@users.noreply.github.com>
2021-02-26 08:25:58 -0500

    Delete reveal_background.js

--- src/reveal_background.js
+++
@@ -1,9 +0,0 @@
-
-(document).ready(function() {
-  var $reveal = $(".reveal"),
-      revealWHalf = $reveal.width() / 2;
-  $(document).on("mousemove", function(e) {
-    $reveal.css({"left": e.pageX - revealWHalf, "top": e.pageY - revealWHalf});
-  });
-});
-
